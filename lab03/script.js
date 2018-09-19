1d5f0c03869193d286ed479ab3f8e76e5054b489
knappna <43099260+knappna@users.noreply.github.com>
2018-09-19 14:51:22 -0400

    Create script.js

---
+++ lab03/script.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//app.get('/', (req, res) => res.send('Natalie is the best!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.use(express.static('public'))
+
+
+console.log("This is a message to console.");
