9d81f521f8dd109aca1c9eb6a75e11b9e93978ae
knappna <43099260+knappna@users.noreply.github.com>
2018-10-19 10:53:54 -0400

    Update script.js

--- lab07/script.js
+++ lab07/script.js
@@ -1,14 +1,32 @@
 $( document ).ready(function() {
 
 
-    $( "#mybutton" ).click(function( event ) {
-
-
-        $("<em>", {html: "Hello, Lab7!"}).appendTo("body");
-
-        event.preventDefault();
-
-    });
-
+    //$( "#mybutton" ).click(function( event ) {
+       // $("<em>", {html: "Hello, Lab7!"}).appendTo("body");
+        //event.preventDefault();
+//    });
+    // Request web content (asynchronously) and handle result using promises.
+    $("#mybutton").click(
+        function () {
+            if ($(this).hasClass("minus")) {
+                //AJAX request issued
+                let jsPromise = Promise.resolve($.ajax({
+                    url: "/hello",
+                    type: "GET",
+                    data: {
+                        name: "lab07"
+                    }
+                }));
+                jsPromise.then(function (result) {
+                    //if AJAX request worked
+                    $("#mybutton").next("div").html("<p>" + result.name + "</p>");
+                }, function (xhr) {
+                    //if it failed
+                    $("#mybutton").next("div").html("<p>" + xhr.statusText + "</p>");
+                });
+            }
+        }
+    );
+         
 });
 
