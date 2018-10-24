f12011c6799d54d398e107d7268858df0ae4bdac
knappna <43099260+knappna@users.noreply.github.com>
2018-10-24 11:44:24 -0400

    Update lab06.js

--- lab06/app/lab06.js
+++ lab06/app/lab06.js
@@ -1,47 +1,62 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const http_status = require('http-status-codes');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-//Creating Router() object
-//const router = express.Router();
+const HOST = "localhost";
+const PORT = 3000;
 
-// Listen to this Port
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 //get
-
 app.get("/request/my-handling-form-page", function (req, res) {
 	res.status(200).send("Got a GET request");
 })
-
 
 //head
 app.head("/request/my-handling-form-page", function (req, res) {
 	res.status(200).send("Got a HEAD request");
 })
 
+
+
+
 //put - use CURL
 //curl -X GET localhost:3000
 //curl -X PUT localhost:3000
-app.put("/request/my-handling-form-page", function (req, res){
-	res.status(200).send("Got a PUT request at /user");
-})
-
+app.put("localhost:3000/request", function (req, res){
+	res.send("Got a PUT request at /request"); //don't need res.status because send() 
+})						//automatically returns http_status.ACCEPTED (200)
 
 //post
-app.post("/request/my-handling-form-page", function (req, res) {
-	res.status(200).send("Got a POST request");
+app.post("localhost:3000/request", function (req, res) {
+	res.send("Got a POST request");
 })
 
 //delete
-app.delete("/request/my-handling-form-page", function (req, res) {
-	res.status(200).send("Got a DELETE request at /user");
+app.delete("localhost:3000/request", function (req, res) {
+	res.send("Got a DELETE request at /request");
 })
 
+
+
+
+
 //forms thing
+///request/my-handling-form-page
 app.post('/forms', function(req, res) {
     res.send('Hello, form POST!<br>Posted message: <code>'
 	     + req.body.user_message + '</code>');
 });
 
 
+// Listen to this Port
+app.listen(PORT, HOST, () => {
+    console.log("listening on " + HOST + ":" + PORT + "...");
+});
