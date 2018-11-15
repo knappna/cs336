2a96617959c70c81bb38d7675141768b40390cd7
knappna <43099260+knappna@users.noreply.github.com>
2018-11-15 13:28:44 -0500

    Add files via upload

---
+++ lab09/app/scripts/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import CommentBox from './commentBox';
+
+import '../css/base.css';
+// JavaScript code
+
+ReactDOM.render(
+  <CommentBox url="/api/comments" pollInterval={2000} />,
+  document.getElementById('content')
+);
+
