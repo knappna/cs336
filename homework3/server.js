f81168fc85a68d64696070772f5ae417d9fa9c2d
knappna <43099260+knappna@users.noreply.github.com>
2018-11-15 15:21:01 -0500

    Update server.js

--- homework3/server.js
+++ homework3/server.js
@@ -1,34 +1,68 @@
-/**
- * This implements some examples using jQuery and AJAX.
- */
-
-const express = require("express")
+/*const express = require("express")
 const app = express();
 const http_status = require("http-status-codes");
 const bodyParser = require("body-parser")
 
 const HOST = "localhost";
 const PORT = 3000;
+*/
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
 
-app.use(express.static("public"));
+var db;
+
+/*app.use(express.static("public"));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));*/
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'dist')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
 
 //people - list of people or add new person based on form
 app.get("/people", function (req, res) {
-	res.status(200).send("Requesting person info");
-})
+	db.collection("Homework3").find({}).toArray(function(err, docs) {
+		if (err) throw err;
+		res.json.(docs);
+	});
+});
 
 app.post("/people", function (req, res) {
-	res.send();
-})
+    var newPerson = {
+        id: req.body.id,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+	start_date: req.body.start_date,
+    };
+    db.collection("Homework3").insertOne(newPerson, function(err, result) {
+        if (err) throw err;
+        db.collection("Homework3").find({}).toArray(function(err, docs) {
+            if (err) throw err;
+            res.json(docs);
+        });
+    });
+});
 
 
 //person/id get, update, or delete person's record
 app.get("/person/id", function (req, res) {
-        res.status(200).send("Requesting person id info");
-})
+	db.collection("Homework3").find({req.body.id}).toArray(function(err, docs) {
+		if (err) throw err;
+		res.json.(docs);
+	});
+});
 
 app.post("/person/id", function (req, res) {
         res.send();
@@ -57,4 +91,12 @@ app.post("/person/id/years", function (req, res) {
 
 app.listen(PORT, HOST, () => {
     console.log("listening on " + HOST + ":" + PORT + "...");
+});
+
+var mongoURL = 'mongodb://cs336:' +
+	       process.env.MONGO_PASSWORD +
+           '@ds155243.mlab.com:55243/cs336';
+MongoClient.connect(mongoURL, function(err, dbConnection) {
+    if (err) throw err;
+    db = dbConnection;
 });
