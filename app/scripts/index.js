a6917232ead57f25dabc7465842cf4384029ebe8
knappna <43099260+knappna@users.noreply.github.com>
2018-11-30 15:08:48 -0500

    Update index.js

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { Router, Route, browserHistory } from 'react-router';
 
-
 import CommentBox from './commentBox';
+import CommentEdit from './commentEdit';
 
 import '../css/base.css';
 // JavaScript code
