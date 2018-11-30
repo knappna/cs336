66d8a4557e0b947c09eace9ee98c82591c0e55fc
knappna <43099260+knappna@users.noreply.github.com>
2018-11-30 15:06:14 -0500

    Update commentForm.js

--- app/scripts/commentForm.js
+++ app/scripts/commentForm.js
@@ -23,23 +23,17 @@ module.exports = React.createClass({
     this.props.onCommentSubmit({author: author, text: text});
     this.setState({author: '', text: ''});
   },
-  render: function() {
-    return (
-      <form className="commentForm" onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          placeholder="Your name"
-          value={this.state.author}
-          onChange={this.handleAuthorChange}
-        />
-        <input
-          type="text"
-          placeholder="Say something..."
-          value={this.state.text}
-          onChange={this.handleTextChange}
-        />
-        <input type="submit" value="Post" />
-      </form>
-    );
-  }
+    render: function() {
+        return (
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input className="ui-widget ui-corner-all" type="text" placeholder="name..."
+                    value={this.state.author} onChange={this.handleAuthorChange}
+                />
+                <input className="ui-widget ui-corner-all" type="text" placeholder="comment..."
+                    value={this.state.text} onChange={this.handleTextChange}
+                />
+                <input className="ui-button ui-widget ui-corner-all" type="submit" value="Post" />
+            </form>
+        );
+    }
 });
